e6417f573164c62528c4efce30f1d441eb90cbbd
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-03-04 06:56:53 +0200

    Add modal

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hello, World");
+
+const showModal = function () {
+  blurBg.style.display = "block";
+  modal.style.display = "block";
+};
+const hideModal = function () {
+  modal.style.display = "none";
+  blurBg.style.display = "none";
+};
+const addButton = document.querySelector(".add");
+const hideButton = document.querySelector(".hide");
+const modal = document.querySelector(".modal");
+const blurBg = document.querySelector(".blur-bg");
+
+addButton.addEventListener("click", showModal);
+hideButton.addEventListener("click", hideModal);
