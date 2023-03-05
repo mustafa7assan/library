7ede0b70e118ec15efa5dfed87f6c5e3eb53ed3b
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-03-05 09:04:51 +0200

    Add toggle between book status

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const Book = function (title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = this.read;
 };
 // Array that hold book objects
 const library = [];
@@ -13,10 +14,12 @@ const addBookToLibrary = function (book) {
   ShowBooks();
 };
 
+// Document Object Model Functions
 const ShowBooks = function () {
   const booksContainer = document.querySelector(".books");
   booksContainer.innerHTML = "";
-  for (const book of library) {
+  for (let i = 0; i < library.length; i++) {
+    const book = library[i];
     const div = `
     <div class="book-card">
     <div class="book-cover">
@@ -27,19 +30,41 @@ const ShowBooks = function () {
       <p class="author">By: ${book.author}</p>
       <p class="pages">${book.pages} page</p>
       <div class="button">
-        <button class="btn edit">Edit</button>
-        <button class="btn delete">Delete</button>
+        <button data-index=${i} class="btn edit">Edit</button>
+        <button data-index=${i}  class="btn delete">Delete</button>
       </div>
     </div>
     <div class="book-read">
-      <button class="btn read">Read</button>
+      <button data-index=${i} class="btn ${
+      book.read ? "read" : "unread"
+    } un-read" > ${book.read ? "Read" : "Unread"}</button>
     </div>
   </div>
     `;
+    // Add Event Listeners to buttons
     booksContainer.insertAdjacentHTML("afterbegin", div);
+    document.querySelector(".un-read").addEventListener("click", readToggle);
   }
 };
-// Dom Function
+
+const getBookData = function (e) {
+  e.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = Number(document.getElementById("pages").value);
+  const read = document.getElementById("read").checked;
+  addBookToLibrary({ title, author, pages, read });
+  hideModal();
+  addBookForm.reset();
+};
+// Toggle book from read to unread and versa visa
+const readToggle = function (e) {
+  const index = Number(e.target.dataset.index);
+  const book = library[index];
+  book.read = !book.read;
+  ShowBooks();
+};
+// Modal
 const showModal = function () {
   blurBg.style.display = "block";
   modal.style.display = "block";
@@ -48,20 +73,14 @@ const hideModal = function () {
   modal.style.display = "none";
   blurBg.style.display = "none";
 };
+
+// Selectors
 const addButton = document.querySelector(".add");
 const hideButton = document.querySelector(".hide");
 const modal = document.querySelector(".modal");
 const blurBg = document.querySelector(".blur-bg");
 const addBookForm = document.querySelector(".add-book-form");
-
+// Events Listener
 addButton.addEventListener("click", showModal);
 hideButton.addEventListener("click", hideModal);
-addBookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = Number(document.getElementById("pages").value);
-  addBookToLibrary({ title, author, pages });
-  hideModal();
-  addBookForm.reset();
-});
+addBookForm.addEventListener("submit", getBookData);
